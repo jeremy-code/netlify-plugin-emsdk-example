74ccde1c5456405093c9586c3f8f63ef892ba13c
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-09-03 01:53:01 -0700

    chore: remove output dir config

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,10 +3,5 @@ import { defineConfig } from "nitro";
 export default defineConfig({
   preset: "netlify-edge",
   serverEntry: "./src/server.ts",
-  output: {
-    dir: "dist",
-    serverDir: "dist/server",
-    publicDir: "dist/public",
-  },
   minify: false,
 });
